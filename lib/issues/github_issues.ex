2a17051d930650a1c8624b2e6aeffaab96e34fc6
Alex Tsiras <tsirasalexis@yahoo.gr>
2019-04-08 18:06:13 +0300

    Add GithubIssues module

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,16 @@
+defmodule GithubIssues do
+  @user_agent [{"User-agent", "Elixir tsirasalexis@yahoo.gr"}]
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  defp issues_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
+
+  defp handle_response(%{status_code: 200, body: body}), do: {:ok, body}
+  defp handle_response(%{status_code: _, body: body}), do: {:error, body}
+end
